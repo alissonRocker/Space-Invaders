0a2af6fad1b8ad4a247c3a0dc0063dbb15190f26
Alisson <rockspringnet@gmail.com>
2015-08-10 21:05:05 -0300

    Configuração do Jogador !

--- core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
+++ core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -1,13 +1,18 @@
 package br.grupointegrado.SpaceInvaders;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 
 public class TelaJogo extends TelaBase {
@@ -17,6 +22,12 @@ public class TelaJogo extends TelaBase {
     private Stage palco;
     private BitmapFont fonte;
     private Label lbPontuacao;
+    private Image jogador;
+    private Texture texturaJogador;
+    private Texture texturaJogadorDireita;
+    private Texture texturaJogadorEsquerda;
+    private boolean indoDireita;
+    private boolean indoEsquerda;
 
     /**
      * Construtor padrão da tela de jogo.
@@ -37,6 +48,20 @@ public class TelaJogo extends TelaBase {
 
         initFonte();
         initInformacoes();
+
+        initJogador();
+    }
+
+    private void initJogador() {
+        texturaJogador = new Texture("sprites/player.png");
+        texturaJogadorDireita = new Texture("sprites/player-right.png");
+        texturaJogadorEsquerda = new Texture("sprites/player-left.png");
+
+        jogador = new Image(texturaJogador);
+        float x = camera.viewportWidth / 2 - jogador.getWidth() / 2;
+        float y = 15;
+        jogador.setPosition(x, y);
+        palco.addActor(jogador);
     }
 
     private void initFonte() {
@@ -54,7 +79,7 @@ public class TelaJogo extends TelaBase {
 
     /**
      * Chamado a todo a quadro de atualização do jogo (FPS - Quadros por segundos)
-     * @param delta Tempo entre um campo e outro (em segundos)
+     * @param delta Tempo entre um quadro e outro (em segundos)
      */
     @Override
     public void render(float delta) {
@@ -63,8 +88,59 @@ public class TelaJogo extends TelaBase {
 
         lbPontuacao.setPosition(10, camera.viewportHeight - 20); // altura mazima da camera - 20.
 
+        capturaTeclas();
+        atualizarJogador(delta);
+
         palco.act(delta); // avisa que ja passou o quadro com determinados segundos.
         palco.draw();
+    }
+
+    /**
+     * Verifica se as teclas estão pressionadas.
+     */
+    private void capturaTeclas() {
+        indoDireita = false;
+        indoEsquerda = false;
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) { indoEsquerda = true; }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) { indoDireita = true; }
+
+    }
+
+    /**
+     * Atualiza a posição do jogador.
+     * @param delta
+     */
+    private void atualizarJogador(float delta) {
+        float velocidade = 200; // Velocidade de movimento do jogador.
+
+        if(indoDireita) {
+            if(jogador.getX() < camera.viewportWidth - jogador.getWidth()) {
+                float x = jogador.getX() + velocidade * delta; // tempo que passou X velocidade.
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+            }
+        }
+
+        if(indoEsquerda) {
+            if(jogador.getX() >= 0) {
+                float x = jogador.getX() - velocidade * delta; // tempo que passou X velocidade.
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+            }
+        }
+
+        if(indoDireita) {
+            // trocar imagem direita.
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogadorDireita)));
+        } else if (indoEsquerda) {
+            // trocar imagem esquerda.
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogadorEsquerda)));
+        } else {
+            // trocar imagem centro.
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogador)));
+        }
+
     }
 
     /**
@@ -102,7 +178,10 @@ public class TelaJogo extends TelaBase {
         batch.dispose();
         palco.dispose();
         fonte.dispose();
-    }
 
+        texturaJogador.dispose();
+        texturaJogadorDireita.dispose();
+        texturaJogadorEsquerda.dispose();
+    }
 
 }
